99549cc6895f54f2ae511ada0d97494184dd473d
TeoRam23 <TeoRam23@vgs.nfk.no>
2023-11-06 13:45:37 +0100

    Begynt mer med lagring av beskjeder

--- server.js
+++ server.js
@@ -9,29 +9,33 @@ const oneHour = 1000 * 60 * 60
 
 var session;
 
-/*var users = [
+var users
+
+var preuserstest = [
     {username: "magne", password: "123"},
     {username: "importer", password: "sussy"},
     {username: "croodmate", password: "baka"},
     {username: "bruker", password: "Passord1"}
-]*/
+]
 
-// HALLO FREMTIDSMEG! dettte er det jeg holdt på med forrige gang, og det virker ikke¨
-//HALLO FREMTIDSMEG!
+fs.writeFile("users.txt", JSON.stringify(preuserstest), (err) => {
+    if (err) console.log(err);
+    console.log("bro ya");
+})
+    
 fs.readFile("users.txt", function(err, buf) { 
     if (err) { console.log(err) }
-    users = [buf.toString()]
-    console.log(users);
+    users = JSON.parse(buf);
 });
 
-var tasks = [
-    {bruker: "bruker", task: "æææ"}, 
-    {bruker: "bruker", task: "ÆÆÆ"}, 
-    {bruker: "bruker", task: "aaa"}, 
-    {bruker: "bruker", task: "AAA"}, 
-    {bruker: "bruker", task: "skanning"}, 
-    {bruker: "bruker", task: "task 1"}
-]
+
+var messages
+
+fs.readFile("messages.txt", function(err, buf) { 
+    if (err) { console.log(err) }
+    messages = JSON.parse(buf);
+    console.log("h")
+});
 
 
 
@@ -108,7 +112,7 @@ app.get('/load_tasks', function (req, res) {
     session = req.session
     if (session.username) {
         res.json({ 
-            tasks: tasks,
+            tasks: messages,
             username: session.username
         }) 
     }
@@ -117,8 +121,8 @@ app.get('/load_tasks', function (req, res) {
 app.delete('/delete/:index', (req, res) => {
     var index = parseInt(req.params.index) // index er en params
     console.log(index)
-    if (index >= 0 && index < tasks.length && tasks[index].bruker == req.session.username) {
-        tasks.splice(index, 1) // sletter EN task fra listen
+    if (index >= 0 && index < messages.length && messages[index].bruker == req.session.username) {
+        messages.splice(index, 1) // sletter EN task fra listen
         res.json({ success: true })
     } else {
         res.json({ success: false })
@@ -131,7 +135,21 @@ app.post('/ask', (req, res) => {
     console.log(task)
     console.log(bruker)
     if (task && bruker){
-        tasks.push({bruker: bruker, task: task})
+        fs.readFile("messages.txt", (err, data) => {
+            if (err) { return console.error(err); };
+
+            var data = JSON.parse(data.toString());
+            data.bruker = bruker;
+            data.task = task;
+            fs.writeFile("messages.txt", JSON.stringify(data), (err, result) => { //HALLO FREMTIDSmEG! HÆÆÆÆæÆÆæææææÆ https://stackoverflow.com/questions/48739630/append-text-to-existing-json-file-node-js
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log(result);
+                console.log("yayÆÆÆÆ")
+            }});
+        });
+        //messages.push({bruker: bruker, task: task})
 
         res.json({  success: true,
         })
